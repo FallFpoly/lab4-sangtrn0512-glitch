a01363e25e5ddb1450a4d873f2ba22e23f3a39a8
sang <sang@172.16.75.66>
2025-10-04 08:48:05 +0700

    nop lab 3

--- Lab4/src/main/java/com/mycompany/lab4/Lab4.java
+++ Lab4/src/main/java/com/mycompany/lab4/Lab4.java
@@ -11,6 +11,6 @@ package com.mycompany.lab4;
 public class Lab4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hello World!");
     }
 }
